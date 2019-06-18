54c3ee107ec9ed0e5fa39122b9e3225dae40ed2a
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-18 16:24:08 +0200

    Update exercise2.js

--- Exercises/exercise2.js
+++ Exercises/exercise2.js
@@ -3,6 +3,28 @@
 var square = number => Math.pow(number, 2);
 console.log(square(5)); 
 
+// Print only the correct emails that finish with .com
+
+
+var emails = ['germancutraro@hotmail.com', 'germancutraro', 'johndoe@gmail.com', 'notAndEmail', 'trucky@gmail'];
+var foo = emails.filter((email) => email.includes('@') && email.endsWith('.com'));
+console.log(foo);
+
+// Print only people who are greater than 18 years old
+var persons = [
+    {name: 'John', age: 27},
+    {name: 'Nicholas', age: 16},
+    {name: 'Adney', age: 15},
+    {name: 'Anton', age: 46},
+  ];
+
+var people = persons.filter(person => person.age > 18);
+console.log(people);
+   
+//Print ONLY THE FIRST even number
+var numbers3 = [5, 7, 33, 88, 92];
+var even = numbers3.find(num => num % 2 === 0);
+console.log(even);
 
 // Get the sum of a array numbers
 var numbers = [5, 5, 5, 10, 30];

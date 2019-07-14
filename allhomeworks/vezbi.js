fd22e36364bd26d5dc4aaaeb89dc9cb05fb2d856
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-07-14 20:50:23 +0200

    new exercises

---
+++ allhomeworks/vezbi.js
@@ -0,0 +1,202 @@
+// 1.Napisete program koj sto ima dve variabli (promenlivi) i ni vrakja koj broj e pogolem od niv. 
+//Vo konzolata treba da pisuva Primer: -> Brojot 5 e pogolem od brojot 3;
+
+
+
+// 2.Napisete program koj sto vrakja text (vo konzolata) koj sto vika: Hello World vo cetiri jazici. Angliski, Francuski, Spanski, Germanski,vo zavisnost od izbraniot jazik vo variablata. Primer -> var lang = "spain" --> Rezultatot treba da bide na spanski.
+
+
+// 3.Napisete program koj sto vi vrakja rezultat dali moze da si kupite nov laptop.
+// Iminjata na laptopite so nivnite ceni se: Macbook, Macbook Pro i Macbook Air. 
+//Cenata na Macbook = $500, na Macbook Pro = $900 i na Macbook Air = $600; Treba da cuvate budzet (odnosno vasi pari vo variabla. Primer -> var budzet = "$500");
+
+
+// 4.Напишете циклус за пресметување на сите парни броеви во еден опсег кој што ќе го внесете од prompt.
+
+
+// 5.Напишете ги сите карти од еден шпил (1-срце, 1-детелина,1-баклава, 1-лист).
+
+// var num = [2,3,4,5,6,7,8,9,10, "J", "Q", "K", "A"];
+// var type = ["Srce", "Detelina", "Baklava", "List"];
+
+
+// 6.Креирајте објект кој во себе ќе има 7 својства со барем 2 низи
+
+
+// //-Од тие две низи извлечете ги и прикажете ги првите и последните елементи
+
+
+// 7.Направете низа од имиња и прикажете ги оние чија должина е 5.
+
+// var names = ["Filip", "Stefan", "Elena", "Stojan", "Aleksandra", "Milos", "Dimitar", "Ljube","Darko", "Petar", "Slavica", "Mile", "Sanja"];
+
+
+
+// 8.Направете низа чија големина ќе ја внесете преку prompt. Сите елементи од таа низа внесете ги преку prompt и прикажете ја подредена.
+
+
+
+// 9.Направете низа библиотека кој ќе содржи елементи кои се објекти. Тие објекти треба да бидат книги кои ќе имаат автор, наслов, и дали е прочитана или не (true/false)
+// -Прикажете кои книги се прочитани а потоа кои не се прочитани.
+
+// console.log("With For Loop");
+// console.log("*****************************");
+
+// 	var library = [
+// 		{
+// 			title: "The Big Sleep",
+// 			author: "Raymond Chandler",
+// 			read: true
+// 		},
+
+// 		{
+// 			title: "American Heroin",
+// 			author: "Melissa Scrivner Love",
+// 			read: false
+// 		},		
+
+// 		{
+// 			title: "The Reckoning",
+// 			author: "John Grisham",
+// 			read: false
+// 		},
+
+// 		{
+// 			title: "The Chalk Man",
+// 			author: "C. J. Tudor",
+// 			read: true
+// 		}
+// 	]
+
+//with for loop!
+
+
+//     console.log("*****************************");
+// 	console.log("Ending of for loop!");
+// 	console.log("*****************************");
+
+
+// //With forEach loop
+
+
+//10. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Sample numbers: 0, -1, 4
+
+//11. Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result. Sample numbers : -5, -2, -6, 0, -1 */
+
+//12. Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+
+//13. Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18"). Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+
+//14. The Grade Assigner
+
+//15. Make a function to print the fibbonaci scale(up to 1000)
+
+//15. Make this pattern:  1 2 4 8 16 32 64 128
+
+//16. Make this pattern: 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4
+
+
+//17. Make this pattern: 1 1 1 2 2 2 3 3 3 4 4 4
+
+
+//18. Make this pattern: 3 6 9 12 15
+
+
+//19. Make this pattern: 9 8 7 6 5 4 3 2 1 0
+
+
+
+//20. Create a program where the last 3 digits of a number are added together		
+
+
+
+//21. Check the length of a name, and tell if the length is even or odd!
+
+
+
+//22. Print odd and even numbers with prompt and save them in a separate array.
+
+
+
+//23. Print all numbers between -10 and 19;
+
+
+
+//24. Print all even numbers between 10 and 40
+
+
+//25. Print all odd numbers between 300 and 333 
+
+//26. Print all numbers dividible by 3 and 5 between 10 and 50.
+
+
+//27. Make a function factorial, 3 ways!
+
+//Function factorial
+
+//FUNCTION FACTORIAL VERSION 2
+
+//28. Make a day on earth calculator!
+
+//29. Make a todo list program, where a prompt will ask you to "quit" , "list", "new".
+
+//30. Make a isEven and kebabToSnake function!
+
+//31. Using fetch, make a table that will generate the following data: http://dummy.restapiexample.com/api/v1/employees
+
+//32. Make these functions areaTriangle, areaRectangle, areaCircle
+
+//33. Make the "guess the secret number" game
+
+//34. Using classes make a class male and female and have a method to tell if they can they.
+// They can date only if they are in the range of 5 years.
+
+//35. Make a calculator on your page!
+
+//36./Од дадената низа, треба да се отпечати име и презиме на студентот со највисок просек,
+//студентот со најнизок просек, и средна вредност (просек) на целата генерација (сите студенти)
+
+// var people = [
+// 	{ime: "Pero", prezime:"Perovski", prosek: 6.2},
+// 	{ime: "Janko", prezime:"Jankovski", prosek: 7.1},
+// 	{ime: "Stanko", prezime:"Stankovski", prosek: 8.3},
+// 	{ime: "Petko", prezime:"Petkovski", prosek: 9.81},
+// 	{ime: "Ivana", prezime:"Ivanovska", prosek: 6},
+// 	{ime: "Gordana", prezime:"Gordanovska", prosek: 6.1},
+// 	{ime: "Cveta", prezime:"Cvetkoska", prosek: 9.2}
+// ];
+
+//37. Create a file with lorem ipsum text inside and read it with node. Print how many characters(including 
+// signs and empty spaces), how many letters are there, how many sentences, words, and words with
+//7 letters, more than 7, and less than 7 characters!
+
+//38. Make a calculator using the url
+
+//39. Finish the last homework from semos (node js!)
+
+//40. Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine 
+//the corresponding grade.
+
+/*Student Name	Marks
+David	80
+Vinoth	77
+Divya	88
+Ishitha	95
+Thomas	68
+The grades are computed as follows :
+
+Range	  Grade
+<60			F
+<70			D
+<80			C
+<90			B
+<100		A
+*/
+
+//41. Write a JavaScript program to construct the following pattern, using a nested for loop. 
+
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * * */

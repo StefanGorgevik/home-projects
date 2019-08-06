b66a1c2b98342d6198e9b880271805ff93add8e5
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-08-06 21:49:27 +0200

    js crash course 1

---
+++ crash-courses/1/index.js
@@ -0,0 +1,87 @@
+//document.getElementById("content").innerHTML = " I am a p tag";
+//document.write("I have been written to the document!");
+
+// var myName = "Stefan";
+// var myAge = 25;
+// console.log(myName);
+// console.log(myAge);
+// var myDOB = "23rd May";
+// var favFilms = ["Pulp Fiction", "Fight Club", "Cast Away"];
+// var likesTea = true;
+// var person = {
+//     name: "Stefan",
+//     age: 25,
+//     city: "Veles",
+//     street: "Hristo Tatarchev 6/3 - 15",
+//     hobbies: ["Music", "Biking", "Reading"]
+// }
+// console.log(person.street);
+// console.log(person.hobbies[2]);
+
+// console.log(JSON.stringify(person));
+// console.log(`${typeof myDOB}`)
+
+// var result = 5 + 3;
+// console.log(result);
+
+// var numA = 5,
+//     numB = 2,
+//     result2 = numA + numB;
+// console.log("result2 is " + result2);
+// result2 = ++numA;
+// console.log(result2);
+
+// var x = 10;
+// console.log(x);
+// x = 5;
+// console.log(x);
+// x += 5;
+// console.log(x);
+
+// function funcName(param1, param2) {
+//     return param1 + param2;
+// }
+// console.log(funcName(3,3));
+
+// var toCelsius = (fahrenheit) =>{ 
+//     return (5/9) * (fahrenheit - 32);
+// }
+// console.log(toCelsius(77));
+
+// var carName = "Rover";
+
+// function returnModel() {
+//     var carModel = "25 GTI";
+// }
+
+// console.log(carName);
+
+// var clicked = () => {
+//     console.log("Clicked");
+// }
+
+// var age = 17;
+// var passed = true;
+
+// if(age >= 17 && !passed) {
+//     console.log("Well Done!")
+// } else if(age < 17) {
+//     console.log("You are not old enough!");
+// }
+// else {
+//     console.log("Sorry! You have not passed your test!");
+// }
+
+// var myNum = 5;
+// while(myNum !== 10) {
+//     myNum = ++myNum;
+//     console.log(myNum);
+// }
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+var myStr = "This is WebsiteBytes\' JS crash course";
+console.log(myStr.search("JS"));
+console.log(myStr.indexOf("JS"));

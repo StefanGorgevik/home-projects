5f49a1957b09aa36d0e8fb2c35b31325e8ab2faa
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-08-09 17:04:14 +0200

    to do list getting there

--- projects/todos/index.js
+++ projects/todos/index.js
@@ -5,8 +5,8 @@ class UI {
         this.showTodoDiv = document.querySelector(".showTodo");
         this.feedbackP = document.querySelector(".feedbackP");
         this.itemDiv = document.querySelector(".itemDiv");
-        this.editButton = document.querySelector(".editButton");
-        this.dltButton = document.querySelector(".dltButton");
+        this.editButton = document.querySelector("#editButton");
+        this.dltButton = document.querySelector("#dltButton");
         this.emptyDiv = document.querySelector(".emptyDiv");
         this.itemsDiv = document.querySelector(".items");
         this.todoList = document.querySelector(".todoList");
@@ -47,7 +47,14 @@ class UI {
     removeTodo(element) {
         var id = parseInt(element.dataset.id);
         console.log(id);
-
+        let parent = element.parentElement.parentElement;
+        console.log(parent);
+        this.itemsDiv.remove(parent);
+        let tempList = this.todoList.filter(function(item) {
+            return item.id !== id;
+          });
+        this.todoList = tempList;
+        console.log(this.todoList);
     }
 
 }
@@ -55,17 +62,26 @@ class UI {
 function eventListeners() {
     var inputTodo = document.querySelector(".inputTodo");
     var submitButton = document.querySelector("#submitButton");
-    var showTodoDiv = document.querySelector(".showTodo");
-
+    var itemsDiv = document.querySelector(".items");
+   
     var ui = new UI();
     submitButton.addEventListener("click", function(event) {
         event.preventDefault();
         ui.submitTodo();
-    })
-    dltButton.addEventListener("click", function() {
-        event.preventDefault();
-        ui.removeTodo(event.target.parentElement);
     });
+    itemsDiv.addEventListener("click", function(event){
+        console.log(event.target.parentElement)
+        ui.removeTodo(event.target.parentElement);
+    })
+
+    
+    // var dltButton = document.querySelector("#dltButton");
+    // dltButton.addEventListener("click", function() {
+    //     event.preventDefault();
+    //     ui.removeTodo(event.target.parentElement);
+    // });
+
+    
     
 }
 
